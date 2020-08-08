7e6761af4e2ceb2e7e0a5381b3317d95afe737b2
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-08-08 07:11:12 -0500

    api

--- petshop/settings.py
+++ petshop/settings.py
@@ -143,33 +143,33 @@ STATICFILES_DIRS = (BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ORIGIN_WHITELIST = [
-    'https://apirestshoop.herokuapp.com/servicios/productos/',
-    'https://apirestshoop.herokuapp.com/servicios/categorias/',
-    'http://localhost:3006'
-]
+# CORS_ORIGIN_WHITELIST = [
+#    'https://apirestshoop.herokuapp.com/servicios/productos/',
+#    'https://apirestshoop.herokuapp.com/servicios/categorias/',
+#    'http://localhost:3006'
+# ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOW_METHODS = [
-#    'DELETE',
-#    'GET',
-#    'OPTIONS',
-#    'PATCH',
-#    'POST',
-#    'PUT',
-# ]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
-# CORS_ALLOW_HEADERS = [
-#    'accept',
-#    'accept-encoding',
-#    'authorization',
-#    'content-type',
-#    'dnt',
-#    'origin',
-#    'user-agent',
-#    'x-csrftoken',
-#    'x-requested-with',
-# ]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
