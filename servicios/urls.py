af4d448f23dc530ba711ae6ea75c81257aea3036
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-11-24 17:56:34 -0500

    correction city

--- servicios/urls.py
+++ servicios/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('categorias', views.CategoriaView,)
-router.register('city', views.CategoriaView,)
+router.register('city', views.CityView,)
 router.register('province', views.ProvinceView,)
 router.register('district', views.DistrictView,)
 router.register('color', views.ColorView,)
