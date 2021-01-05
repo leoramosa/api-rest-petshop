12d34d68b21569bf4ef355217710a4ae36a8738a
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-01-05 16:21:16 -0500

    cambio de admin lista

--- servicios/admin.py
+++ servicios/admin.py
@@ -3,24 +3,24 @@ from .models import Carritocompras, Cliente, Color, Comentario, Foto, Genero, Ma
 from .models import Historialconsulta, Tipopago, Tipoproducto, Usuario, Valoracion, Venta, VentaProducto
 from .models import Producto, Categoria, City, Province, District, Imagencolor
 
-admin.site.register(Carritocompras)
-admin.site.register(Cliente)
+""" admin.site.register(Carritocompras) """
+""" admin.site.register(Cliente) """
 admin.site.register(Color)
 admin.site.register(Talla)
-admin.site.register(Comentario)
-admin.site.register(Foto)
-admin.site.register(Genero)
-admin.site.register(Marca)
-admin.site.register(Modelo)
-admin.site.register(Pago)
+""" admin.site.register(Comentario) """
+""" admin.site.register(Foto) """
+""" admin.site.register(Genero) """
+""" admin.site.register(Marca) """
+""" admin.site.register(Modelo) """
+""" admin.site.register(Pago) """
 admin.site.register(Producto)
-admin.site.register(Historialconsulta)
-admin.site.register(Tipopago)
-admin.site.register(Tipoproducto)
+""" admin.site.register(Historialconsulta) """
+""" admin.site.register(Tipopago) """
+""" admin.site.register(Tipoproducto) """
 admin.site.register(Usuario)
-admin.site.register(Valoracion)
-admin.site.register(Venta)
-admin.site.register(VentaProducto)
+""" admin.site.register(Valoracion) """
+""" admin.site.register(Venta) """
+""" admin.site.register(VentaProducto) """
 admin.site.register(Categoria)
 admin.site.register(City)
 admin.site.register(Province)
